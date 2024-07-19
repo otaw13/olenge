78567482faec1a922bed89216013089fbc3f1243
Your Name <you@example.com>
2024-07-19 19:11:59 +0500

    final

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   close.addEventListener("click", function () {
     sidebar.classList.remove("translate-x-0");
-    sidebar.classList.toggle("translate-x-100");
+    sidebar.classList.add("translate-x-full");
   });
+
   menuButton.addEventListener("click", function () {
     sidebar.classList.toggle("translate-x-0");
+    sidebar.classList.toggle("translate-x-full");
+  });
+
+  const closeSidebar = () => {
+    sidebar.classList.remove('translate-x-0');
+    sidebar.classList.add('translate-x-full');
+  };
+
+  document.querySelectorAll('#sidebar nav a').forEach(link => {
+    link.addEventListener('click', closeSidebar);
   });
 });
